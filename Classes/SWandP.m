eff4873b0317aa46df8a201a2c32a3626bd52a28
Mikhail Vaganov <mikhail.vaganov.sci@gmail.com>
2016-03-23 03:24:16 +0500

    Created a new class for hybrid soft matter

---
+++ Classes/SWandP.m
@@ -0,0 +1,87 @@
+classdef SWandP  < iMagneticParticle
+    % Complex particle consisting of Stoner-Wohlfarth particle and
+    % paramagnetic particle described by the second model.
+    %
+    % If interaction is strong, then the soft particle get magnetizatization 
+    % the same sign as hard particle has - it means, that the field of the 
+    % hard particle in the place where soft particle is located is greater, 
+    % than the external field!    
+    %
+    % This is the third model of SW+soft structural particle
+    %
+    
+    properties
+        SWparticle;
+        StrongInteraction = false;
+        PSusceptibility=1;
+        Gamma1=0.8;
+        Gamma2=0.8;
+        SaturationField=2;
+    end
+    
+    methods
+        function r = SWandP(sw)
+            r.SWparticle = sw;
+        end;
+        
+        function r = SetUp(p)
+            p.SWparticle = p.SWparticle.ApplyField(p.SaturationField);
+            P_m = p.ParamagnetM(p.SaturationField);
+            p.Magnetization=p.SWparticle.Magnetization+P_m;
+            r=p;
+        end;
+        
+        function r = SetDown(p)
+            p.SWparticle = p.SWparticle.ApplyField(-p.SaturationField);
+            P_m = p.ParamagnetM(-p.SaturationField);
+            p.Magnetization=p.SWparticle.Magnetization+P_m;
+            r=p;
+        end;
+        
+        function m = ParamagnetM(p, field)
+            p.SWparticle = p.SWparticle.ApplyField(field);
+            m = p.PSusceptibility*(field + p.Gamma1*p.SWparticle.Magnetization);
+        end;
+        
+        function r = ApplyField(p,field)
+            P_m = p.ParamagnetM(field);
+            p.SWparticle = p.SWparticle.ApplyField(field + p.Gamma2*P_m);
+            p.Magnetization = p.SWparticle.Magnetization + P_m;
+            r = p;
+        end;
+        
+        function h = PositiveSaturationField(p)
+            h = p.SaturationField;
+        end;
+        
+        function h = NegativeSaturationField(p)
+            h = - p.SaturationField;
+        end;
+        
+        function Draw(p,folder)
+            t=0:0.01:10;
+            magnitude=p.PositiveSaturationField;
+            input = -magnitude*cos(t);
+            
+            output=zeros(length(t),1);
+            for i=1:1:length(t);
+                p = p.ApplyField(input(i));
+                output(i) = p.Magnetization;
+            end;
+            
+            plot(input,output,'b.');
+            title(['Hyseresis of SW+Soft particle. SW_psi=', num2str(p.SWparticle.AngleFA)]);
+            
+            mkdir(folder);
+             file_name = [...
+                 'SW+Soft(' ...
+                 num2str(p.SWparticle.AngleFA) ...
+                 ')____' ...
+                 datestr(now,'HH_MM_SS') ...
+                 ];
+            print('-djpeg',[folder file_name]); 
+        end;
+    end
+    
+end
+
--- Sandbox/SingleSWSoftParticleMatter.m
+++ Sandbox/SingleSWSoftParticleMatter.m
@@ -2,10 +2,10 @@ clc;
 close all;
 
 sw = SWparticle(pi/2,1);
+sw_p = SWandP(sw);
 
-
-for i=1:1:length(sw)
-    SHMatter = SingleParticleMatter(sw(i));
+for i=1:1:length(sw_p)
+    SHMatter = SingleParticleMatter(sw_p(i));
     SHForc = FORC(SHMatter);
     SHForc.MagnetizationFORC();
 end;
